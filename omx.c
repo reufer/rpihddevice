f9b1bcff899ffb505ec3e65bed6aea22bf9d22a5
Thomas Reufer <thomas@reufer.ch>
2014-03-16 20:52:25 +0100

    fixed OMX shutdown when no stream has been played (reported by Klaus Schmidinger)

--- omx.c
+++ omx.c
@@ -349,6 +349,9 @@ int cOmx::DeInit(void)
 {
 	Cancel();
 
+	for (int i = 0; i < eNumTunnels; i++)
+		ilclient_disable_tunnel(&m_tun[i]);
+
 	ilclient_teardown_tunnels(m_tun);
 	ilclient_state_transition(m_comp, OMX_StateIdle);
 	ilclient_state_transition(m_comp, OMX_StateLoaded);
